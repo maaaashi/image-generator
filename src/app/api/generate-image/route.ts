62e7f023f098cbc1be1fc54f96f5f2ffd3b8e720
maaaashi <doyaseizin.7@gmail.com>
2023-09-03 10:09:55 +0900

    動くところまで

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -3,16 +3,22 @@ import { generateAsync } from 'stability-client'
 
 export const POST = async (req: NextRequest) => {
   const { prompt } = await req.json()
+
   try {
     const result = await generateAsync({
       prompt,
       apiKey: process.env.NEXT_PUBLIC_API_KEY!,
       outDir: 'public/generated',
     })
+
     console.log(result)
+
+    // @ts-ignore
+    const images = result.images
+
+    return NextResponse.json({ images })
   } catch (e) {
     console.log(e)
+    return NextResponse.json({})
   }
-
-  return NextResponse.json({})
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,57 @@
+'use client'
+
+import Image from 'next/image'
+import { FormEvent, useState } from 'react'
+
 export default function Home() {
-  return <main></main>
+  const [promptState, setPromptState] = useState('Cats owned by wealthy people')
+  const [imagePath, setImagePath] = useState('')
+  const submitHandler = async (e: FormEvent) => {
+    e.preventDefault()
+
+    const results = await fetch('/api/generate-image', {
+      method: 'POST',
+      body: JSON.stringify({
+        prompt: promptState,
+      }),
+    })
+
+    const json = await results.json()
+
+    const str = json.images[0].filePath
+    const regex =
+      /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}.*\.png)$/
+
+    const match = str.match(regex)
+    if (match) {
+      setImagePath('/generated/' + match[1])
+    } else {
+      console.log('No match found')
+    }
+  }
+
+  return (
+    <main className='container mx-auto'>
+      <form onSubmit={submitHandler} className='flex'>
+        <textarea
+          className='textarea textarea-bordered'
+          placeholder='Type Prompt'
+          value={promptState}
+          onChange={(e) => {
+            setPromptState(e.target.value)
+          }}
+          required
+        ></textarea>
+        <button className='btn btn-primary self-end' onSubmit={submitHandler}>
+          画像生成！
+        </button>
+      </form>
+
+      {imagePath ? (
+        <Image src={imagePath} alt='generate-image' width={400} height={400} />
+      ) : (
+        <></>
+      )}
+    </main>
+  )
 }
