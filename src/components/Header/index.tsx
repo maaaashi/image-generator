3e22f15e6502e9b9787c76a95d231b6830d17da9
maaaashi <doyaseizin.7@gmail.com>
2023-09-03 14:59:20 +0900

    title変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <div className='bg-base-200 p-3'>
-      <h1 className='font-bold text-lg'>Image Generator</h1>
+      <h1 className='font-bold text-lg'>{"Maaaashi's Image Generator"}</h1>
     </div>
   )
 }
