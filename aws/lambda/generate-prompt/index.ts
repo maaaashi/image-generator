d58c38bfc1874d0d1be39294ba920fa3399a1334
maaaashi <doyaseizin.7@gmail.com>
2023-09-07 07:57:13 +0900

    lambda実装

---
+++ aws/lambda/generate-prompt/index.ts
@@ -0,0 +1,48 @@
+import { Handler } from 'aws-lambda'
+import { Configuration, OpenAIApi } from 'openai'
+
+const postGPT = async (prompt: string, draw: string) => {
+  const apiKey = process.env.NEXT_PUBLIC_GPT_API_KEY
+  const configuration = new Configuration({
+    apiKey,
+  })
+  const openai = new OpenAIApi(configuration)
+  const model = 'gpt-4'
+
+  const content = `画像生成AIを使って、${draw}「${prompt}」を書くための英語のプロンプトを考えてください。`
+
+  return await openai.createChatCompletion({
+    model,
+    temperature: 2,
+    messages: [
+      {
+        role: 'system',
+        content,
+      },
+    ],
+  })
+}
+
+type Input = {
+  prompt: string,
+  draw: string
+}
+
+export const handler: Handler<Input, string> = async (req) => {
+  const { prompt, draw } = req
+
+  try {
+    const response = await postGPT(prompt, draw)
+    const content = response.data.choices[0].message?.content!
+    console.log(`${draw}「${prompt}」 is ${content}`)
+
+    return JSON.stringify({
+      content
+    });
+  } catch (e) {
+    console.log(e)
+    return JSON.stringify({
+      content: 'ERROR'
+    });
+  }
+}
--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path = require('path');
 
-export class AwsStack extends cdk.Stack {
+export class MaaaashiImageGenerator extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Function(this, 'ImageGeneratorGeneratePrompt', {
+      functionName: 'ImageGeneratorGeneratePrompt',
+      runtime: Runtime.NODEJS_18_X,
+      code: Code.fromAsset(path.join(__dirname, '../lambda/generate-prompt/')),
+      handler: 'index.handler',
+    });
   }
 }
