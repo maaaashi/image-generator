be10b586744cfa6e98f49bfa0fcb9d7d812f4cdd
maaaashi <doyaseizin.7@gmail.com>
2023-09-07 08:19:16 +0900

    デバッグ

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -7,6 +7,8 @@ export class MaaaashiImageGenerator extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    console.log('apikey is ' + process.env.CHATGPT_APIKEY)
+
     new Function(this, 'ImageGeneratorGeneratePrompt', {
       functionName: 'ImageGeneratorGeneratePrompt',
       runtime: Runtime.NODEJS_18_X,
