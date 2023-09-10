de93e0034c0828f8f6d092b709b9479e04c7edb1
maaaashi <doyaseizin.7@gmail.com>
2023-09-10 21:28:59 +0900

    prompt向上

--- aws/lambda/generate-prompt/index.ts
+++ aws/lambda/generate-prompt/index.ts
@@ -9,7 +9,30 @@ const postGPT = async (prompt: string, draw: string) => {
   const openai = new OpenAIApi(configuration)
   const model = 'gpt-4'
 
-  const content = `画像生成AIを使って、${draw}「${prompt}」を書くための英語のプロンプトを考えてください。`
+  const systemContent = `
+Please come up with an English prompt to draw the user's desired picture using the image generation AI.
+
+The prompt guide is as follows
+
+1. **Basics of Prompt Engineering**
+  - Prompt engineering involves using words to instruct image generation in the world of AI art.
+  - Core Prompt: Concisely describes the central theme or subject (e.g., Panda, a warrior with a sword).
+  - Style: Specifies the style of the prompt (e.g., Realistic, Oil painting, Pencil drawing, Concept art).
+
+2. **Using Artists**
+  - You can add an artist's name to the prompt to emulate a specific artist's style (e.g., in the style of Picasso).
+
+3. **Finishing Touches**
+  - Instructions to add additional elements or specific characteristics to the image. Examples include: Detailed, Surrealism, Trending on ArtStation, etc.
+
+4. **Prompt Weighting**
+  - A technique to increase or decrease the intensity of specific elements in an image. For instance, to modulate the strength of certain colors or objects.
+
+5. **Negative Prompting**
+  - A technique to specify elements you don't want to be generated. For example, to exclude deformed hands or too many fingers.
+
+According to the guide, prompt engineering can significantly improve the quality and composition of the generated images.
+`
 
   return await openai.createChatCompletion({
     model,
@@ -17,14 +40,18 @@ const postGPT = async (prompt: string, draw: string) => {
     messages: [
       {
         role: 'system',
-        content,
+        content: systemContent,
+      },
+      {
+        role: 'user',
+        content: `${draw}「${prompt}」`,
       },
     ],
   })
 }
 
 type Input = {
-  prompt: string,
+  prompt: string
   draw: string
 }
 
@@ -37,12 +64,12 @@ export const handler: Handler = async (req) => {
     console.log(`${draw}「${prompt}」 is ${content}`)
 
     return JSON.stringify({
-      content
-    });
+      content,
+    })
   } catch (e) {
     console.log(e)
     return JSON.stringify({
-      content: 'ERROR'
-    });
+      content: 'ERROR',
+    })
   }
 }
